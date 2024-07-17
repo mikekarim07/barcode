a0f85886b11e8f5eb176a1138c13bffe852403f3
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-07-17 12:13:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -42,12 +42,12 @@ from openpyxl import load_workbook
 # Initialize the camera
 cap = cv2.VideoCapture(0)
 
-# Load the Excel database
-workbook = load_workbook('database.xlsx')
-sheet = workbook.active
+# # Load the Excel database
+# workbook = load_workbook('database.xlsx')
+# sheet = workbook.active
 
-# Get the column index for the code
-code_column = 'B'
+# # Get the column index for the code
+# code_column = 'B'
 
 while True:
     # Capture a frame from the camera
@@ -60,25 +60,12 @@ while True:
     if qr_codes:
         # Extract the decoded data
         qr_data = qr_codes[0].data.decode('utf-8')
-        print("QR Code Data:", qr_data)
-
-        # Search for the QR code as a number in the Excel database
-        code = None  # Initialize code as None
-        for row in sheet.iter_rows(values_only=True):
-            if str(qr_data) in map(str, row):
-                row_index = row[0]
-                code_cell = code_column + str(row_index)
-                try:
-                    code = sheet[code_cell].value
-                except Exception as e:
-                    print(f"Error accessing cell {code_cell}: {e}")
-                else:
-                    break
+        st.write("QR Code Data:", qr_data)
         
         if code is not None:
-            print("hhehehehehe The member code is:", code)
+            st.write("hhehehehehe The member code is:", code)
         else:
-            print("The member is not found.")
+            st("The member is not found.")
 
     # Display the frame
     cv2.imshow('QR Code Scanner', frame)
